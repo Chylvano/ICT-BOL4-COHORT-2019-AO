13a523467500764445ef93bccdfa3ae155baa943
chylvano <chylvanolandburg@gmail.com>
2019-11-29 14:34:43 +0100

    game

--- Week 11 tm 13/adventuregame-template/adventure.js
+++ Week 11 tm 13/adventuregame-template/adventure.js
@@ -5,8 +5,9 @@ document.getElementById("description").innerHTML = "Klik op start om te beginnen
 document.getElementById("button2").style.display = "none";
 document.getElementById("button3").style.display = "none";
 document.getElementById("inventoryItem").style.display = "none";
+document.body.style.backgroundImage = "url('img/start.jpg')";
+var munt = 0
 
-img.src= "img/start.jpg"
 function startspel(){
     document.getElementById("title").innerHTML = "Level 1";
     document.getElementById("description").innerHTML = "Je bent in de woestijn wakker geworden zonder spullen wat ga je zoeken?<br> 1 - zoeken naar water<br> 2 - zoeken naar een stad<br> 3 - zoeken naar mensen";
@@ -33,10 +34,17 @@ function level2a(){
 function level2b(){
     document.body.style.backgroundImage = "url('img/stad.jpg')";
     document.getElementById("title").innerHTML = "Level 2";
-    document.getElementById("description").innerHTML = "Je hebt de stad bereikt.<br>1 - naar een winkel gaan<br>2 - de stad verkennen<br>3 - naar het vliegveld gaan.";
-    document.getElementById("button1").onclick = function() {level2a3d()};
-    document.getElementById("button2").onclick = function() {level2b3e()};
-    document.getElementById("button3").onclick = function() {level2c3f()};
+    document.getElementById("description").innerHTML = "Je hebt de stad bereikt.<br>1 - de stad verkennen <br>2 - naar een winkel gaan<br>3 - naar het vliegveld gaan.";
+    document.getElementById("button1").onclick = function() {level3a()};
+    document.getElementById("button2").onclick = function() {level3b()};
+    document.getElementById("button3").onclick = function() {level3c()};
+    document.getElementById("button1").style.display = "inline-block";
+    document.getElementById("button2").style.display = "inline-block";
+    document.getElementById("button3").style.display = "inline-block";
+    document.getElementById("button1").innerHTML = "1";
+    document.getElementById("button2").innerHTML = "2";
+    document.getElementById("button3").innerHTML = "3";
+
 }
 
 function level2c(){
@@ -50,11 +58,98 @@ function level2c(){
 
 }
 
-function level2b3e(){
+function level3a(){
     document.getElementById("title").innerHTML = "Level 3";
-    document.getElementsByClassName("myImg")[0].src = "img/stadvliegveld.gif";
+    document.getElementById("inventoryItem").style.display = "inline-block";
+    var munt = 1
+    document.getElementById("description").innerHTML = "Je hebt een munt gevonden.";
+    document.getElementById("button1").innerHTML = "Terug naar de stad";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("button1").onclick = function() {level2b()};
 }
 
-function level2a3d(){
-    document.getElementsByClassName("myImg")[0].src = "img/winkel.jpg!d";
+function level3b(){
+    document.getElementById("title").innerHTML = "Level 3";
+    document.body.style.backgroundImage = "url('img/winkel.jpg!d')";
+    document.getElementById("button3").onclick = function() {level2b()};
+    document.getElementById("description").innerHTML = "Je bent bij een winkel<br> 1 - banaan kopen<br> 2 - vliegticket kopen<br> 3 - terug naar de stad";
+    document.getElementById("button1").onclick = function() {winkel1()};
+    document.getElementById("button2").onclick = function() {level5b()};
+    document.getElementById("button3").onclick = function() {level2b()};
+}
+
+function level3c(){
+    if (inventoryItem == 1
+        ){
+     levelv2();
+    }
+    else{
+        levelv1();
+    }
+   }
+
+function level4a(){
+    document.getElementById("title").innerHTML = "Level 4";
+    
+}
+
+function levelv1(){
+    document.getElementById("button1").innerHTML = "Terug naar de stad";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("title").innerHTML = "Helaas";
+    document.getElementById("description").innerHTML = "Je hebt geen ticket dus je kan niet naar binnen";
+    document.getElementById("button1").onclick = function() {level2b()};  
+}
+
+function levelv2(){
+    
+}
+
+function winkel1(){
+    if (inventoryItem == 1
+        ){
+     einde2();
+    }
+    else{
+        geengeld();
+    }
+   }
+
+function einde2(){
+    document.getElementById("title").innerHTML = "Helaas";
+    document.getElementById("button1").onclick = function() {startspel()};
+    document.getElementById("description").innerHTML = "De banaan was giftig nu ben je dood.";
+    document.getElementById("button1").innerHTML = "Opnieuw beginnen";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+}
+
+function geengeld(){
+    document.getElementById("title").innerHTML = "Helaas";
+    document.getElementById("button1").onclick = function() {level2b()};
+    document.getElementById("description").innerHTML = "Je hebt niet genoeg geld.";
+    document.getElementById("button1").innerHTML = "terug naar de stad";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+}
+
+function level5b(){
+    if (inventoryItem == 1
+        ){
+     ticket1();
+    }
+    else{
+        geengeld();
+    }
+   }
+
+function ticket1(){
+    document.getElementById("title").innerHTML = "Gekocht";
+    document.getElementById("button1").onclick = function() {level2b()};
+    document.getElementById("description").innerHTML = "Je hebt een vliegticket gekocht.";
+    document.getElementById("button1").innerHTML = "terug naar de stad";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
 }
