1dbdbb383f83540d33ba0798c1705bd45e506c14
chylvano <chylvanolandburg@gmail.com>
2019-12-13 12:11:50 +0100

    lingo

--- Week 14 tm 17/lingo-nl/lingo-nl.js
+++ Week 14 tm 17/lingo-nl/lingo-nl.js
@@ -479,5 +479,23 @@ var words = [
 	"zever",
 	"zeeen"];
 
+	function newgame(){
 
+	}
+
+	function check(){
+		var textbox = document.getElementById('woord');
+		var woord = textbox.value;
+		var woordArray = woord.split('');
+		var letter = document.getElementById('letter').value;
+	
+		for(i=0; i< woordArray.length; i++){
+			if(woordArray[i] == letter){
+				var kop = document.createElement('h1');
+				kop.innerText = 'letter' + letter + ' gevonden op plek ' + i;
+				document.body.appendChild(kop);
+			}
+		}
+	}
+	
 	
