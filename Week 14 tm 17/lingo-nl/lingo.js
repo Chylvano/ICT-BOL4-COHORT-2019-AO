356876a0819f092ed4ef901930bd81e04c634119
chylvano <chylvanolandburg@gmail.com>
2020-01-17 14:41:01 +0100

    lingo af?

--- Week 14 tm 17/lingo-nl/lingo.js
+++ Week 14 tm 17/lingo-nl/lingo.js
@@ -2,7 +2,8 @@ var vakjes;
 var attempts = 0
 const row1 = document.getElementById("row1");
 
-for(vakjes = 0; vakjes < 25; vakjes++){
+for(row = 0; row < 5; row++){
+    for(col = 0; col < 5; col++){
     var newDiv = document.createElement("input");
     row1.appendChild(newDiv);
     newDiv.style.fontFamily = 'Lato', 'sans-serif';
@@ -13,46 +14,65 @@ for(vakjes = 0; vakjes < 25; vakjes++){
     newDiv.style.textAlign = 'center';
     newDiv.style.fontSize = '3.5em';
     newDiv.style.display = "inline-block";
-    newDiv.id += 'vakje'+ vakjes;
+    newDiv.id += 'vakje'+ row + col;
     newDiv.style.background = "white";
+    }
 }
 
 var woord = words[Math.floor(Math.random() * words.length)];
 
 var currentRow = 1;
 
-
-
 console.log(woord)
 
+var wordArray = woord.split("");
+document.getElementById("vakje00").value = wordArray[0];
+document.getElementById("vakje00").style.backgroundColor = "green";
+console.log(wordArray) 
 
-var textArray = woord.split("");
-document.getElementById("vakje0").value = textArray[0];
-console.log(textArray) 
-
-if(guess.length == 5){
-   alert ("het woord moet 5 letters zijn")
+function green(){
+    document.getElementById("vakje00").style.backgroundColor = "green";
 }
 
 function checkword(){
+    if(woord == guess) {
+        alert("gefeliciteerd je hebt het woord geraden!")
+    }
+    textArray = woord.split('');
     var guess = document.getElementById("guess");
     var guess = guess.value.split('');
-        for (i = 0; i < woord.length; i++) {
-            if(woord[i] == guess[i]) {
-                document.getElementById("vakje" + i).value = guess[i];
-                document.getElementById("vakje" + i).style.backgroundColor = "green";
-                guess[i] = "*";
-            } else if (woord.indexOf(guess[i])>-1) {
-                document.getElementById("vakje" + i).value = guess[i];
-                document.getElementById("vakje" + i).style.backgroundColor = "yellow";
+    console.log(guess)
+
+        for (i = 0; i < 5; i++) {
+            document.getElementById("vakje" + attempts + i).value = guess[i];
+        }
+        
+        for (i = 0; i < 5; i++) {
+            if(textArray[i] == guess[i]) {
+                document.getElementById("vakje" + attempts + i).style.backgroundColor = "yellow";
+                document.getElementById("vakje00").style.backgroundColor = "green";
+                guess[i] = "-";
+                textArray[i] = "*";
+            } 
+        
+        for (i = 0; i < 5; i++) {    
+            if (guess[i] != "-") {
+            if (textArray.indexOf(guess[i])>0) {
+                document.getElementById("vakje" + attempts + i).style.backgroundColor = "green";
+
+                var pos = textArray.indexOf(guess[i]);
+                guess[pos] = "/";
+                textArray[i] = "*";
             } else {
-                document.getElementById("vakje" + i).value = guess[i];
-                document.getElementById("vakje" + i).style.backgroundColor = "red";
-            }
-            if (attempts == 4) {
-                alert("Helaas je hebt het word niet geraden.")
+                document.getElementById("vakje" + attempts + i).style.backgroundColor = "red";
             }
         }
+            if (attempts == 4) {
+                alert("Helaas je hebt het woord niet geraden.")
+            }
+        }
+        attempts++
     }                     
+}
 
- 
+console.log(checkword)
